f3476c8ccef3de0aff9130fff4d7ff078623c8cb
wndgur0425 <rodemtree1009@naver.com>
2018-05-08 13:00:18 +0900

    [2018-05-08] TestNuxt_Webservice

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+    <div class="block">
+   
+      <p>Test about</p>
+      
+    </div>
+    
+  </section>
+</template>
+
+<script>
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+    <div class="block">
+      <a href="#" class="button is-primary">Test</a><br>
+      <p>클라우드 서비스용 포트폴리오</p>
+      <img src="~static/art.jpg"><br>
+      <img src="~assets/customArt.jpg"><br>
+    </div>
+    <br>
+    <nuxt-link to="/about">About</nuxt-link>
+    
+  </section>
+</template>
+
+<script>
+
+</script>
